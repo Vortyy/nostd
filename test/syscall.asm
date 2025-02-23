77393f4bbf6fac9bc26c04a8db1b9050c06a3e2a
vorty <uf4vortex@gmail.com>
2025-02-23 22:54:15 +0100

    testing mmap vs brk

--- test/syscall.asm
+++ test/syscall.asm
@@ -1,5 +1,6 @@
 extern main
 
+; cat /proc/id/maps
 section .text
 
 global _start, syscall5, allocate
@@ -8,7 +9,7 @@ _start:
   xor rbp, rbp
   pop rdi
   mov rsi, rsp 
-  call allocate 
+  call main
 
   mov rdi, rax
   mov rax, 60
@@ -30,14 +31,15 @@ allocate:
   mov rdi, 0
   syscall
 
-  ;; NOTE : [] work as ptr references purely as C does [] <=> * <=> derefecing
+  ;; NOTE : [] mov for var work as ptr references purely as C does [] <=> * <=> derefecing
   ;; NOTE : it's impossible change the addr of data element mov brk_firstpos, rax (forbidden)
   ;; here we store at the address 0x4025 the value of the first address of the heap
   ;; so brk_firstpos -> addr: 0x402500) val: 0x403000
   mov qword [brk_firstpos], rax
 
+  ;; lea take the always the address of  
   ;; allocation with syscall brk
-  lea rdi, [rax + 3]
+  lea rdi, [rax + 5000] 
   mov rax, 12
   syscall
 
@@ -51,6 +53,37 @@ allocate:
   mov rax, [brk_firstpos]
   ret
 
+;; ONLY diff between brk/mmap is memory fragmentation pb 
+mmap_alloc:
+  mov rax, 9 ; SYS_mmap 
+  mov rdi, 0 ; addr NULL
+  mov rsi, 50; size 1 pages 
+  mov rdx, 0x3 ; prot READ_WRITE = 3 = 0x1 | 0x2 (READ | WRITE)
+  mov r10, 0x22; flag MAP_ANON = 32 = 0x20
+  mov r8, -1; fd
+  mov r9 , 0; offset 
+  syscall
+
+  mov qword [brk_firstpos], rax
+
+  ;; we change content inside the value of the addr 0x403000
+  mov rsi, brk_firstpos
+  mov rdi, [brk_firstpos]
+  mov byte [rdi], 'f'
+  mov byte [rdi + 1], 'u'
+  mov byte [rdi + 2], 0
+
+  ;; munmap --> unmap the complete pages
+  mov rax, 11 ; SYS_munmap
+  mov rdi, [brk_firstpos]
+  mov rsi, 50
+  syscall
+
+  ret
+
+
+section .bss
+brk_firstpos: resq 1
   
 ;;.data section add a padding of 4096 byte aligned address ??? seem to be a page
 section .data
@@ -58,4 +91,3 @@ section .data
 ;the last number is append to end of string 
 ;here -> 10 = uincode '\n'
 message: db "test", 10 
-brk_firstpos: dq 0 
